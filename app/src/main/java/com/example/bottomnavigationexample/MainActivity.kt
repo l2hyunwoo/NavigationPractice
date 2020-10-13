153383645e3ee08b3702f2bdd55d7820472834bd
Hyun Woo Lee <l2hyunwoo@gmail.com>
2020-10-13 16:42:59 +0900

    assets and graph setting

--- app/src/main/java/com/example/bottomnavigationexample/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigationexample/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.bottomnavigationexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        NavigationUI.setupWithNavController(bnv_main, findNavController(R.id.nav_host))
     }
 }
